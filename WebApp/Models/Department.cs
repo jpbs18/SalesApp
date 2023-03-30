234e1bc3a6d85e25a806ec186a04f28eb3d82e55
João Santos <joao.santos@mindswap.academy>
2023-03-30 21:27:25 +0100

    Database running with app

--- WebApp/Models/Department.cs
+++ WebApp/Models/Department.cs
@@ -2,13 +2,7 @@
 {
     public class Department
     {
-        public long Id { get; private set; }
-        public string Name { get; private set; }
-
-        public Department(long id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+        public long Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -37,7 +37,8 @@ namespace WebApp
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebAppContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebAppContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebAppContext"), builder => 
+                    builder.MigrationsAssembly("WebApp")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
