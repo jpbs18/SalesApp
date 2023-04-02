d9bd8a483bab6c9b54db4c1622c1a694a90f1da1
João Santos <joao.santos@mindswap.academy>
2023-04-02 19:07:08 +0100

    Finished SimpleSearch

--- WebApp/Controllers/SalesRecordsController.cs
+++ WebApp/Controllers/SalesRecordsController.cs
@@ -1,17 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using WebApp.Services;
 
 namespace WebApp.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year -5, 1, 1);
+            }
+
+            if (!maxDate.HasValue)
+            {
+                maxDate = new DateTime(DateTime.Now.Year - 5, 12, 31);
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
         public IActionResult GroupingSearch()
---
+++ WebApp/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class SalesRecordService
+    {
+        private readonly WebAppContext _context;
+
+        public SalesRecordService(WebAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = _context.SalesRecord.Select(record => record);
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(record => record.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(record => record.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(record => record.Seller)
+                .Include(record => record.Seller.Department)
+                .OrderByDescending(record => record.Date)
+                .ToListAsync();
+        }
+    }
+}
